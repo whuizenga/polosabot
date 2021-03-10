8ae2d36e10076c6f5cdbfead9bf7f86377a6f27f
William Huizenga <will.huizenga@gmail.com>
2021-03-09 20:24:15 -0500

    fix cross server emoji fetching

--- commands/reactionRole.js
+++ commands/reactionRole.js
@@ -2,42 +2,42 @@ const roles = {
   ffxiv: {
     name: 'Final Fantasy XIV',
     roleId: '647602091954798592',
-    emojiText: 'ffxiv',
+    emojiId: '<:ffxiv:818980250733117481>',
   },
   gi: {
     name: 'Genshin Impact',
     roleId: '762401905590009866',
-    emojiText: 'gi',
+    emojiId: '<:gi:818980262696321044>',
   },
   vh: {
     name: 'Valheim',
     roleId: '812069893113511989',
-    emojiText: 'vh',
+    emojiId: '<:vh:818980267440078859>',
   },
   acnh: {
     name: 'Animal Crossing',
     roleId: '701502327412097065',
-    emojiText: 'acnh',
+    emojiId: '<:acnh:818984760998101012>',
   },
   wow: {
     name: 'World of Warcraft',
     roleId: '641367893828960256',
-    emojiText: 'wow',
+    emojiId: '<:wow:818980256408010773>',
   },
   wowclassic: {
     name: 'WoW Classic',
     roleId: '818970275181756477',
-    emojiText: 'wowclassic',
+    emojiId: '<:wowclassic:818981156393058306>',
   },
   ow: {
     name: 'Overwatch',
     roleId: '818970072361205760',
-    emojiText: 'ow',
+    emojiId: '<:ow:818980254575362049>',
   },
   lol: {
     name: 'League of Legends',
     roleId:'818969917499768842',
-    emojiText: 'lol',
+    emojiId: '<:lol:818980264083980318>',
   },
 }
 
@@ -47,14 +47,22 @@ const reactionRole = {
   execute: async (message, args, Discord, client) => {
     const channel = process.env.REACTION_CHANNEL_ID
 
+    const guild = message.guild
+
+    if (!guild.available) return
+
     Object.values(roles).forEach((role) => {
-      role.role = message.guild.roles.cache.find(r => r.id === role.roleId)
-      role.emoji = message.guild.emojis.cache.find(e => e.name === role.emojiText)
-      console.log(role.emoji)
+      const guildRole = message.guild.roles.cache.find(r => r.id === role.roleId)
+
+      if (!guildRole) {
+        console.log(`Failed to find Role for ${role.name}`)
+      } else {
+        role.role = guildRole
+      }
     })
 
     const roleMessages = Object.values(roles).map(role => (
-      `**<:${role.emojiText}:${role.emoji.id}> ${role.name}**`
+      `**${role.emojiId} ${role.name}**`
     ))
 
     const embed = new Discord.MessageEmbed()
@@ -67,7 +75,7 @@ const reactionRole = {
     const messageEmbed = await message.channel.send(embed)
 
     Object.values(roles).forEach((role) => {
-      messageEmbed.react(role.emoji)
+      messageEmbed.react(role.emojiId)
     })
 
     client.on('messageReactionAdd', async(reaction, user) => {
@@ -77,8 +85,12 @@ const reactionRole = {
       if (!reaction.message.guild) return
 
       if (reaction.message.channel.id === channel) {
-        if (reaction.emoji.name === (roles[reaction.emoji.name] && roles[reaction.emoji.name].emojiText)) {
-          await reaction.message.guild.members.cache.get(user.id).roles.add(roles[reaction.emoji.name].role)
+        if (roles[reaction.emoji.name]) {
+          const role = roles[reaction.emoji.name]
+
+          if (role.emojiId.includes(reaction.emoji.id)) {
+            await reaction.message.guild.members.cache.get(user.id).roles.add(role.role)
+          }
         }
       }
     })
@@ -90,8 +102,14 @@ const reactionRole = {
       if (!reaction.message.guild) return
 
       if (reaction.message.channel.id === channel) {
-        if (reaction.emoji.name === (roles[reaction.emoji.name] && roles[reaction.emoji.name].emojiText)) {
-          await reaction.message.guild.members.cache.get(user.id).roles.remove(roles[reaction.emoji.name].role)
+        if (reaction.message.channel.id === channel) {
+          if (roles[reaction.emoji.name]) {
+            const role = roles[reaction.emoji.name]
+  
+            if (role.emojiId.includes(reaction.emoji.id)) {
+              await reaction.message.guild.members.cache.get(user.id).roles.remove(role.role)
+            }
+          }
         }
       }
     })
