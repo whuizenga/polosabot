33d3b342a8bba5826da7df2a6de2d137505c5326
Chris Huizenga <cdhuizenga@gmail.com>
2022-09-07 23:48:38 -0400

    Update reactionRole.js

--- commands/reactionRole.js
+++ commands/reactionRole.js
@@ -9,11 +9,6 @@ const gameRoles = {
     roleId: '762401905590009866',
     emojiId: '<:gi:818980262696321044>',
   },
-  vh: {
-    name: 'Valheim',
-    roleId: '812069893113511989',
-    emojiId: '<:vh:818980267440078859>',
-  },
   acnh: {
     name: 'Animal Crossing',
     roleId: '701502327412097065',
@@ -27,40 +22,27 @@ const gameRoles = {
   pokemon: {
     name: 'Pokémon',
     roleId:'881390854521114655',
-<<<<<<< HEAD
     emojiId: '<:pokemon:888218743451291679>',
-=======
-    emojiId: '<:pokemon:888209291343986738>',
->>>>>>> a65f50a425bcfd7187f4742743aa186d8ce76348
   },
-  mc: {
-    name: 'Minecraft',
-    roleId: '888183801140903996',
-<<<<<<< HEAD
-    emojiId: '<:mc:888218698899419146>',
+  fhx: {
+    name: 'FHX',
+    roleId:'1014242805939449867',
+    emojiId: '<:fhx:1017276973669679145>',
   },
-}
-
-const gamerRole = '836630061675184179'
-
-const ffxivRoles = {
   pf: {
-    name: 'Party Finder',
+    name: 'FFXIV: Party Finder',
     roleId: '841727270976225280',
     emojiId: '<:pf:888218795750092820>',
   },
   expert: {
-    name: 'Expert Roulette',
+    name: 'FFXIV: Expert Roulette',
     roleId: '888199752305483876',
     emojiId: '<:expert:888218874875633734>',
   },
   roulettes: {
-    name: 'Roulettes',
+    name: 'FFXIV: Roulettes',
     roleId: '883924511827836928',
     emojiId: '<:roulettes:888218838276132894>',
-=======
-    emojiId: '<:mc:888205163385470996>',
->>>>>>> a65f50a425bcfd7187f4742743aa186d8ce76348
   },
 }
 
@@ -95,7 +77,7 @@ const reactionRole = {
         .setDescription(
           'React below to get a role\n\n' + roleMessages.join('\n')
         )
-
+  
       const messageEmbed = await message.channel.send(embed)
 
       Object.values(gameRoles).forEach((role) => {
@@ -147,88 +129,5 @@ const reactionRole = {
     })
   }
 }
-
-const ffxivReactionRole = {
-  name: 'ffxivreactionrole',
-  description: 'Sets up the ffxiv reaction role message',
-  execute: async (message, createMessage = false, Discord, client) => {
-    const channel = process.env.REACTION_CHANNEL_ID
-
-    const guild = client.guilds.cache.get(process.env.SERVER_ID)
-
-    Object.values(ffxivRoles).forEach((role) => {
-      const guildRole = guild.roles.cache.find(r => r.id === role.roleId)
-
-      if (!guildRole) {
-        console.log(`Failed to find Role for ${role.name}`)
-      } else {
-        role.role = guildRole
-      }
-    })
-
-    if (createMessage || (message.channel && message.channel.id !== channel)) {
-      const roleMessages = Object.values(ffxivRoles).map(role => (
-        `**${role.emojiId} ${role.name}**`
-      ))
-
-      const embed = new Discord.MessageEmbed()
-        .setColor('#e42643')
-        .setTitle('FFXIV Roles')
-        .setDescription(
-          'React below to get a role\n\n' + roleMessages.join('\n')
-        )
-  
-      const messageEmbed = await message.channel.send(embed)
-
-      Object.values(ffxivRoles).forEach((role) => {
-        messageEmbed.react(role.emojiId)
-      })
-    }
-
-    client.on('messageReactionAdd', async(reaction, user) => {
-      if (reaction.message.partial) await reaction.message.fetch()
-      if (reaction.partial) await reaction.fetch()
-      if (user.bot) return
-      if (!reaction.message.guild) return
-
-      if (reaction.message.channel.id === channel) {
-        if (ffxivRoles[reaction.emoji.name]) {
-          const role = ffxivRoles[reaction.emoji.name]
-
-          if (role.emojiId.includes(reaction.emoji.id)) {
-            await reaction.message.guild.members.cache.get(user.id).roles.add(role.role)
-            await reaction.message.guild.members.cache.get(user.id).roles.add(gamerRole)
-          }
-        }
-      }
-    })
-
-    client.on('messageReactionRemove', async(reaction, user) => {
-      if (reaction.message.partial) await reaction.message.fetch()
-      if (reaction.partial) await reaction.fetch()
-      if (user.bot) return
-      if (!reaction.message.guild) return
-
-      if (reaction.message.channel.id === channel) {
-        if (reaction.message.channel.id === channel) {
-          if (roles[reaction.emoji.name]) {
-            const role = roles[reaction.emoji.name]
-
-            if (role.emojiId.includes(reaction.emoji.id)) {
-              await reaction.message.guild.members.cache.get(user.id).roles.remove(role.role)
-              if(reaction.message.guild.members.cache.get(user.id).roles.cache.some(role=>['ffxiv','wow','acnh','gi','vh','mc','pokemon',].includes(role.name)) === true)  {
-                  await reaction.message.guild.members.cache.get(user.id).roles.add(gamerRole)
-                }
-                else {
-                  await reaction.message.guild.members.cache.get(user.id).roles.remove(gamerRole)
-                }
-            }
-          }
-        }
-      }
-    })
-  }
-}
-
 
 module.exports = reactionRole
